b529a431867ce8abc1c06183ce60f6b98c22a93b
Carlos Lopez <celm000@gmail.com>
2021-06-18 01:29:13 -0700

    Reverting to cbf898b

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,20 @@
 const express = require('express');
+
 const router = express.Router();
 
-const Landlord = require('../models/landlord');
+const { getLandlords } = require('../models/landlord');
 
 // Config Import
 const KEY = process.env.MAPSAPI;
 
 // index
-router.get("/", async (req, res, next) => {
+router.get('/', async (req, res, next) => {
     try {
-        const landlords = await Landlord.find().exec();
+        const landlords = await getLandlords();
 
         res.status(200).render('landing', {
-            landlords: landlords,
-            KEY: KEY
+            landlords,
+            KEY,
         });
     } catch (err) {
         console.log('Broken.. /index GET');
